faa08d10b9055c6273fc9e74f6678d2cdaae952f
Gwen Shapira <cshapi@gmail.com>
2022-05-06 02:42:47 -0700

    draft: very very preliminary example of control plane using Nile

---
+++ mt_control_plane/agent/agent-python-serverless/control-plane-lambda.py
@@ -0,0 +1,57 @@
+import json
+import boto3
+import os
+from lib import nile
+from datetime import datetime
+
+url = os.environ.get('NILE_URL', 'http://localhost:8080')
+#ID is temporary, until we route by workspace name or entire base urls for a workspace
+workspace_id = os.environ.get('NILE_WORKSPACE_ID','4')
+username = os.environ.get('NILE_TENANT_USERNAME','gwen@demo.com')
+password = os.environ.get('NILE_TENANT_PASSWORD','verysecret')
+mysql_default_password =  os.environ.get('MYSQL_DEFAULT_PWD','changeme')
+
+nile = nile.NileClient(url, workspace_id)
+rds = boto3.client('rds')
+
+
+def handler(event):
+    updates = {}
+    if event['status'] == 'create_requested':
+        new_cluster =  rds.create_db_cluster(
+            Engine='aurora-mysql',
+            DBClusterIdentifier=event['cluster_name'],
+            MasterUserPassword=mysql_default_password,
+            MasterUsername="root")
+        updates['status'] = new_cluster['DBCluster']['Status']
+        updates['Endpoint'] = new_cluster['DBCluster']['Endpoint']
+        updates['ARN'] = new_cluster['DBCluster']['DBClusterArn']
+        updates['updated'] = str(datetime.now())
+        return updates
+    if event['status'] == 'delete_requested':
+        deleted = rds.delete_db_cluster(
+            DBClusterIdentifier=event['cluster_name'],
+            SkipFinalSnapshot=True)
+        updates['status'] = deleted['DBCluster']['Status']
+        updates['updated'] = str(datetime.now())
+        return updates
+
+def checker(event):
+    updates = {}   
+    try:
+        clusters = rds.describe_db_clusters(DBClusterIdentifier=event['cluster_name'])
+    except rds.exceptions.DBClusterNotFoundFault:
+        return None
+    updates['status'] = clusters['DBClusters'][0]['Status']
+    updates['updated'] = str(datetime.now())
+    return updates
+
+
+if __name__ == "__main__":
+    access_token = nile.login(username,password)
+    (success, fail) = nile.handle_events('clusters-v2', access_token, handler, checker, 'create_requested','delete_requested')
+    print ("events handled successfully: ")
+    print (*success, sep="\n")
+    print ("errors encountered: ")
+    print (*fail, sep="\n")
+
---
+++ mt_control_plane/control-plane-python/app/__init__.py
@@ -0,0 +1,36 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    # for session encryption
+    app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # Initializing Nile client for a specific workspace
+    # developer credentials are used only to set up the workspace and create entities
+    from . import nile
+    url = os.environ.get('NILE_URL', 'http://localhost:8080')
+    workspace = os.environ.get('NILE_WORKSPACE','control-plane')
+    key = os.environ.get('NILE_KEY','set-dev-credentials')
+    secret = os.environ.get('NILE_SECRET','set-dev-credentials')
+    nile._nile_client = nile.NileClient(url, workspace, key, secret)
+    
+    # register command to init the entities:
+    from . import setup
+    setup.init_app(app)
+
+    # register blueprints with the app views
+    from . import auth, clusters
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(clusters.bp)
+    app.add_url_rule('/', endpoint='index')
+
+    return app
