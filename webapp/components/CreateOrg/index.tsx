932c2079a1c786dd7331a0b46418e85d7ccc52e8
Yeva Byzek <yeva@thenile.dev>
2022-10-11 19:12:35 -0400

    THE-728: checkpoitn

--- webapp/components/CreateOrg/index.tsx
+++ webapp/components/CreateOrg/index.tsx
@@ -13,70 +13,25 @@ import NavBar from '../NavBar';
 import paths from '~/paths';
 
 export default function AddOrgForm() {
-  const nile = useNile();
   const router = useRouter();
-  const { register, handleSubmit } = useForm();
-  const [error, setError] = React.useState<null | string>();
   const { publicRuntimeConfig } = getConfig();
   const { NILE_ENTITY_NAME } = publicRuntimeConfig;
-  const mutation = useMutation(
-    (data: CreateOrganizationRequest) =>
-      nile.organizations.createOrganization({
-        createOrganizationRequest: data,
-      }),
-    {
-      onSuccess: (data) => {
-        if (!NILE_ENTITY_NAME) {
-          alert('no entity type has been entered.');
-        } else {
-          router.push(
-            paths.entities({ org: data.id, entity: NILE_ENTITY_NAME }).index
-          );
-        }
-      },
-      onError: (e: Error) => {
-        if (typeof e.message === 'string') {
-          setError(e.message);
-        }
-      },
-    }
-  );
-
-  const handleUpdate = React.useCallback(
-    async (data: CreateOrganizationRequest) => {
-      setError(null);
-      mutation.mutate(data);
-    },
-    [mutation]
-  );
 
   return (
     <NavBar>
-      <Stack spacing={2} mt={3}>
-        <Typography level="h5">Organization name</Typography>
-        <Stack
-          component="form"
-          spacing={1}
-          onSubmit={handleSubmit((data) =>
-            handleUpdate(data as CreateOrganizationRequest)
-          )}
-        >
-          <TextField
-            {...register('name')}
-            sx={{ maxWidth: '30rem' }}
-            id="name"
-            name="name"
-            label="Name"
-            required
-            helperText={<>{error && <Alert severity="error">{error}</Alert>}</>}
-            onChange={() => setError(null)}
-            error={Boolean(error)}
-          />
-          <Box>
-            <Button type="submit">Create organization</Button>
-          </Box>
-        </Stack>
-      </Stack>
+      <Stack>
+        <Typography level="h5">
+          Yo, you new around here?
+        </Typography>
+        <OrganizationForm
+          onSuccess={(data) => {
+            router.push(
+              paths.entities({ org: data.id, entity: NILE_ENTITY_NAME }).index
+            );
+          }}
+          cancelButton={<Button variant="outlined">Cancel</Button>}
+        />
+      />
     </NavBar>
   );
 }
