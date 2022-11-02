375b3b1bf4775f0d25c91c8e95e3b5e45731606c
Yeva Byzek <yeva@thenile.dev>
2022-11-02 13:13:54 -0400

    Checkpoint

--- metrics/src/index.ts
+++ metrics/src/index.ts
@@ -1,4 +1,4 @@
-import { Measurement, Metric, MetricTypeEnum } from '@theniledev/js';
+import Nile, { NileApi, Measurement, Metric, MetricTypeEnum } from '@theniledev/js';
 
 var exampleUtils = require('../../utils-module-js/').exampleUtils;
 
@@ -31,20 +31,42 @@ async function generateMetrics() {
   // Login
   nile = await exampleUtils.loginAsDev(nile, NILE_URL, NILE_WORKSPACE, process.env.NILE_DEVELOPER_EMAIL, process.env.NILE_DEVELOPER_PASSWORD, process.env.NILE_WORKSPACE_ACCESS_TOKEN);
 
+  // List entities
+  var myEntities =  await nile.entities.listEntities()
+  if (myEntities.find( ws => ws.name==NILE_ENTITY_NAME)) {
+      console.log(emoji.get('dart'), "Entity " + NILE_ENTITY_NAME + " exists");
+  } 
+
+  var oneInstance;
+  await nile.entities.listInstancesInWorkspace({
+      type: NILE_ENTITY_NAME,
+    }).then((data) => {
+      for (let i=0; i < data.length; i++) {
+        console.log(`${data[i].id}`);
+        oneInstance = data[i].id;
+      }
+    });
+
+  // Produce one metric
   const metricName = "myMetric";
   const intervalTimeMs = 2000;
   const fakeMeasurement = {
     timestamp: new Date(),
-    value: 1,
-    instanceId: "instance_id",
+    //timestamp: "2022-11-13T20:20:39Z",
+    value: 11.8,
+    //instance_id: oneInstance,
+    instanceId: oneInstance,
   };
 
   const metricData = {
     name: metricName,
     type: MetricTypeEnum.Gauge,
+    //entity_type: NILE_ENTITY_NAME,
     entityType: NILE_ENTITY_NAME,
     measurements: [fakeMeasurement],
   };
+
+  console.log(`\n\nSending metric:\n[${JSON.stringify(metricData, null, 2)}]`);
 
   await nile.metrics.produceBatchOfMetrics({
     metric: [metricData],
