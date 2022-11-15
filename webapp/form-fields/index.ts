80fde8d4c8cd9b98880692c1767b6a2f6ec6d456
Yeva Byzek <ybyzek@users.noreply.github.com>
2022-11-15 09:50:56 -0500

    fix(webapp): simplify webapp code for form-fields and metrics (#127)

--- webapp/form-fields/index.ts
+++ webapp/form-fields/index.ts
@@ -1,26 +1,16 @@
 import { Attribute } from '@theniledev/react';
 
-import * as DbFields from './DB';
-import * as SkyNet from './SkyNet';
-import * as Banking from './Banking';
-import * as Workload from './Workload';
-
 type Fields = {
   fields: Attribute[];
   columns: string[];
   instanceName: string;
 };
 export const getFormFields = (entity: string): void | Fields => {
-  if (entity === 'DB') {
-    return DbFields;
-  }
-  if (entity === 'SkyNet') {
-    return SkyNet;
-  }
-  if (entity === 'Banking') {
-    return Banking;
-  }
-  if (entity === 'Workload') {
-    return Workload;
+  try {
+    const entityModuleFormFields = require(`./${entity}/index.ts`);
+    return entityModuleFormFields;
+  } catch (err) {
+    console.error(err);
+    console.error(`Did you check that webapp/form-fields/${entity}/index.ts exists?`);
   }
 };
--- webapp/metrics/index.ts
+++ webapp/metrics/index.ts
@@ -1,24 +1,14 @@
-import * as DBMetrics from './DB';
-import * as SkyNetMetrics from './SkyNet';
-import * as BankingMetrics from './Banking';
-import * as WorkloadMetrics from './Workload';
-
 type Metrics = {
   gaugeGraph: string[];
   lineChart: string[];
   averageNum: string[];
 };
 export const getMetrics = (entity: string): void | Metrics => {
-  if (entity === 'DB') {
-    return DBMetrics;
-  }
-  if (entity === 'SkyNet') {
-    return SkyNetMetrics;
-  }
-  if (entity === 'Banking') {
-    return BankingMetrics;
-  }
-  if (entity === 'Workload') {
-    return WorkloadMetrics;
+  try {
+    const entityModuleMetrics = require(`./${entity}/index.ts`);
+    return entityModuleMetrics;
+  } catch (err) {
+    console.error(err);
+    console.error(`Did you check that webapp/metrics/${entity}/index.ts exists?`);
   }
 };
